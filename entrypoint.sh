0cef2ba79dd30f9385b25ecd08318cf4e45fbfaa
coolasice1999 <52601736+coolasice1999@users.noreply.github.com>
2021-11-14 22:27:06 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 
 if [ ! -f /mnt/cron.conf ]; then
 #set time to 12:30AM run for movies and 12:45AM run for tvshows
-    echo "30 0 * * * /config/tinyMediaManager.sh movie -u --scrapeUnscraped" > /mnt/cron.conf
-    echo "45 0 * * * /config/tinyMediaManager.sh tvshow -u --scrapeUnscraped" > /mnt/cron.conf
+    echo -e "30 0 * * * /config/tinyMediaManager.sh movie -u --scrapeUnscraped\n45 0 * * * /config/tinyMediaManager.sh tvshow -u --scrapeUnscraped" > /mnt/cron.conf
 fi
 chmod 777 /config/*
 chmod 600 /mnt/cron.conf
